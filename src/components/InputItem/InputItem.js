7d24039a9abfca2426dd06f18df1d33ca84b91c5
Коционова Анна <kotsionova@yandex.ru>
2020-10-09 20:36:06 +0300

    Добавлена стилизация инпута через material-ui

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (
-  <input type="text" name="input" placeholder="Что нужно сделать?" value="" />
+const InputItem = () => (<div>
+  <TextField
+  id="standard-full-width"
+  style={{ margin: 8 }}
+  placeholder="Что нужно сделать?"
+  fullWidth
+  margin="normal"
+  InputLabelProps={{
+    shrink: true,
+  }}
+  />
+  </div>
 );
 
 export default InputItem;
