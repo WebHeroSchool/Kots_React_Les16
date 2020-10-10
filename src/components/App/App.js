cff02a9df2f54af1c1bda1f3dad852b4ad30daa7
Коционова Анна <kotsionova@yandex.ru>
2020-10-10 14:58:05 +0300

    App сделан компонентом класса

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,27 +4,31 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css'
 
-const deals = [{
-  id: 1,
-  isDone: true,
-  value: 'Встать'
-},{
-  id: 2,
-  isDone: false,
-  value: 'Посидеть'
-},{
-  id: 3,
-  isDone: true,
-  value: 'Лечь'
-}];
-const dealNumber = deals.length;
+class App extends React.Component {
+  render() {
+    const deals = [{
+      id: 1,
+      isDone: true,
+      value: 'Встать'
+    },{
+      id: 2,
+      isDone: false,
+      value: 'Посидеть'
+    },{
+      id: 3,
+      isDone: true,
+      value: 'Лечь'
+    }];
+    const dealNumber = deals.length;
 
-const App = () => (<div className={styles.wrap}>
-  <h1 className={styles.title}>TODOs</h1>
-  <InputItem />
-  <ItemList deals={deals}/>
-  <Footer dealNumber={dealNumber}/>
-  </div>
-);
+    return  (<div className={styles.wrap}>
+      <h1 className={styles.title}>TODOs</h1>
+      <InputItem />
+      <ItemList deals={deals}/>
+      <Footer dealNumber={dealNumber}/>
+      </div>
+    );
+  }
+}
 
 export default App;
