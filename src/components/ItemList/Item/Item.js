da8d6a9ac0cac46965f4abf78918aa01a7265ffd
Коционова Анна <kotsionova@yandex.ru>
2020-10-11 14:27:25 +0300

    Добавлен пример с утечкой данных

--- src/components/ItemList/Item/Item.js
+++ src/components/ItemList/Item/Item.js
@@ -11,15 +11,11 @@ import PropTypes from 'prop-types';
 
 class Item extends React.Component {
   componentDidMount() {
-    console.log('componentDidMount');
-  }
-
-  shouldComponentUpdate() {
-    console.log('shouldComponentUpdate');
+    this.timerId = setInterval(() => console.log('interval'), 1000);
   }
 
   componentWillUnmount() {
-    console.log('componentWillUnmount');
+    clearInterval(this.timerId);
   }
 
   render() {
