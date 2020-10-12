e6829ebe22ae7dcdc54e13833fa281977e22e063
Коционова Анна <kotsionova@yandex.ru>
2020-10-12 20:14:55 +0300

    Добавлено подтягивание данных о пользователе с GitHub

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,16 +1,69 @@
 import React from 'react';
-import styles from './AboutMe.module.css'
+import Card from '@material-ui/core/Card';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import styles from './AboutMe.module.css';
 
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit();
 
-const AboutMe = () => {
+class AboutMe extends React.Component {
+  state = {
+    isLoading: true,
+    reposList: [],
+    userInfo: {}
+  }
 
-  return  (
-    <div className={styles.wrap}>
-    <h1 className={styles.title}>Обо мне</h1>
-    <span className={styles.label}>Анна Коц - очень старательный человек</span>
-    <img src={require("./img/busy.jpg")} alt="busy" style={{margin: "2%", width: "98%"}}/>
-    </div>
-  );
+  componentDidMount() {
+    octokit.repos.listForUser({username: 'AnnaKots',})
+    .then(data => {
+      this.setState({
+        reposList: data.data,
+        isLoading: false
+      });
+    })
+    .catch(err => console.log(err));
+
+    octokit.users.getByUsername({
+      username: 'AnnaKots',
+    }).then(data => {
+      this.setState({
+        userInfo: data.data
+      })
+    });
+  }
+
+  render() {
+    return (
+      <Card>
+      <div className={styles.wrap}>
+      <div>
+      {
+        this.state.isLoading
+        ? <div className={styles.progress}><CircularProgress/></div>
+        : <div>
+        <h1 className={styles.title}>Обо мне</h1>
+        <div className={styles.info}>
+        <div><h3 className={styles.inline}>Имя: </h3>
+        <span className={styles.text}>{this.state.userInfo.name}</span></div>
+        <div><h3 className={styles.inline}>Город: </h3>
+        <span className={styles.text}>{this.state.userInfo.location}</span></div>
+        <div><h3 className={styles.inline}>Информация о себе: </h3>
+        <span className={styles.text}>{this.state.userInfo.bio}</span></div>
+        </div>
+        <img src={this.state.userInfo.avatar_url} alt='busy' style={{width: 200}}/>
+        <h3 className={styles.title}>Мои репозитории</h3>
+        <ol className={styles.list}>
+        {this.state.reposList.map(repo =>
+          (<li key={repo.id}><a href={repo.html_url} className={styles.link}>{repo.name}</a></li>)
+        )}
+        </ol>
+        </div>
+      }
+      </div>
+      </div>
+      </Card>
+    );
+  }
 }
 
 export default AboutMe;
